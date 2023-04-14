c197520d4dbd0195e6f73d0be03efc601d42c662
Eric Nguyen <11a1eric3@gmail.com>
2023-04-14 10:13:12 +0700

    ex4: update calendar

--- exerciseDay4/Calendar/index.js
+++ exerciseDay4/Calendar/index.js
@@ -1,7 +1,13 @@
-let dayOfWeek = 3;
-let totalDays = 30;
 let currentDay = new Date();
 const dayTable = document.getElementById("daytable");
+const daytimeInput = document.getElementById("daytime");
+const monthSelectElement = document.getElementById('month');
+const yearSelectElement = document.getElementById('year');
+const doubleBackBtn = document.getElementById('doubleBack');
+const backBtn = document.getElementById('back');
+const nextBtn = document.getElementById('next');
+const doubleNextBtn = document.getElementById('doubleNext');
+
 
 const months = [
   "January",
@@ -18,7 +24,19 @@ const months = [
   "December",
 ];
 
+months.forEach((m, i) => {
+  const newmon = document.createElement('option');
+  newmon.innerHTML = m;
+  newmon.setAttribute('value', i)
+  monthSelectElement.appendChild(newmon);
+});
 
+for (let i = 1900; i < 2100; i++) {
+  const newyear = document.createElement('option');
+  newyear.innerHTML = '' + i;
+  newyear.setAttribute('value', i)
+  yearSelectElement.appendChild(newyear);
+}
 
 const createBlankDay = () => {
   const blankDay = document.createElement("li");
@@ -52,11 +70,25 @@ const createNormalDay = (daynum, isActive = false) => {
   return normalDay;
 };
 
+/**
+ * rerenderControllers
+ */
+const rerenderControllers = () => {
+  daytimeInput.value = currentDay;
+  monthSelectElement.value = currentDay.getMonth();
+  yearSelectElement.value = currentDay.getFullYear();
+}
+
+/**
+ * 
+ * @param {Date} value 
+ */
 const renderDayOfMonth = (value) => {
   dayTable.innerHTML = "";
   const firstDay = new Date(value.getFullYear(), value.getMonth(), 0);
+  const lastDay = new Date(value.getFullYear(), value.getMonth() + 1, 0);
   const dow = firstDay.getDay() + 1;
-  const tt = firstDay.getDate();
+  const tt = lastDay.getDate();
   for (let blank = 0; blank < dow; blank++) {
     dayTable.appendChild(createBlankDay());
   }
@@ -68,19 +100,50 @@ const renderDayOfMonth = (value) => {
   for (let blank = tt; blank <= 42; blank++) {
     dayTable.appendChild(createBlankDay());
   }
+  rerenderControllers()
 };
-
-renderDayOfMonth(currentDay);
 
 const handleChangeInputDay = (e) => {
-  console.log(e.target.value);
   const value = new Date(e.target.value);
-  // const firstDay = new Date(value.getFullYear(), value.getMonth(), 0);
-  // const dow = firstDay.getDay() + 1;
-  // const tt = firstDay.getDate();
-  // console.log("dow", dow);
-  // console.log("tt", tt);
-  renderDayOfMonth(value);
+  currentDay = value;
+  renderDayOfMonth(currentDay);
 };
-const daytimeInput = document.getElementById("daytime");
+
+/**
+ * 
+ * @param {number} e 
+ */
+const handleChangeOptionMonth = (e) => {
+  if (e.target.value) {
+    currentDay.setMonth(e.target.value);
+    renderDayOfMonth(currentDay);
+  }
+}
+
+/**
+ * 
+ * @param {number} e 
+ */
+const handleChangeOptionYear = (e) => {
+  if (e.target.value) {
+    currentDay.setFullYear(e.target.value);
+    renderDayOfMonth(currentDay);
+  }
+}
+
+/**
+ * @param {number} nMonth
+ */
+const handleClickControllerBtn = (nMonth) => {
+  currentDay.setMonth(currentDay.getMonth() + nMonth);
+  renderDayOfMonth(currentDay);
+}
+
 daytimeInput.addEventListener("change", handleChangeInputDay);
+monthSelectElement.addEventListener("change", handleChangeOptionMonth);
+yearSelectElement.addEventListener("change", handleChangeOptionYear);
+doubleBackBtn.addEventListener("click", () => handleClickControllerBtn(-2));
+backBtn.addEventListener("click", () => handleClickControllerBtn(-1));
+doubleNextBtn.addEventListener("click", () => handleClickControllerBtn(1));
+nextBtn.addEventListener("click", () => handleClickControllerBtn(2));
+renderDayOfMonth(currentDay);
