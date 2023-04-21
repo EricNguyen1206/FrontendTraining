42c57ce3bdb3ae1bfef130d00f1576a47c6c7c86
Eric Nguyen <11a1eric3@gmail.com>
2023-04-21 09:17:14 +0700

    W2Exercise1: complete logic carosel

--- W2exercise1/src/ts/index.ts
+++ W2exercise1/src/ts/index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+let slideIndex: number = 0;
+
+function nextSlide() {
+    slideIndex++;
+    if (slideIndex >= document.getElementsByClassName("banner__item").length) {
+        slideIndex = 0;
+    }
+    updateCarousel();
+}
+
+function updateCarousel() {
+    let carouselInner = Array.from(
+        document.getElementsByClassName(
+            "banner__list"
+        ) as HTMLCollectionOf<HTMLElement>
+    )[0];
+    carouselInner.style.transform = `translateX(-${slideIndex * 50}%)`;
+}
+
+// Set initial slide
+updateCarousel();
+
+setInterval(nextSlide, 3000);
